1827479115d3c2500edbb2f1bd78e84519ab5308
Eirik Ølberg <eirik.olberg@kantega.no>
2023-04-16 16:12:48 +0200

    In localStorage saving of email and score

--- src/components/stage/Ferdig.tsx
+++ src/components/stage/Ferdig.tsx
@@ -1,7 +1,57 @@
-import { useContext } from "react";
+import {useContext, useEffect, useState} from "react";
 import { AppContext } from "../../pages/_app";
 import { Introduction } from "../atoms/Introduction"
 import styles from './Ferdig.module.css';
+import {ButtonSubmit} from "../atoms/ButtonSubmit";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
+
+interface ScoreEntry {
+  nickname: string,
+  time: number
+}
+interface Scoreboard {
+  [email: string]: ScoreEntry
+}
+
+function Konkurranseskjema() {
+  const context = useContext(AppContext);
+
+  const [scoreboard, setScoreboard] = useState<Scoreboard>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined"){
+      const toParse = localStorage.getItem("scoreboard")
+      setScoreboard(() => JSON.parse(toParse || '{}'));
+    }
+  }, [scoreboard])
+
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const handleButtonClick = (): void => {
+    if (typeof window !== "undefined" && scoreboard){
+      scoreboard[email] = {
+        nickname: nickname,
+        time: context?.time || 0,
+      }
+
+      localStorage.setItem("scoreboard", JSON.stringify(scoreboard));
+      context?.goToNextPage();
+    }
+  }
+
+  return (
+    <div>
+      <p>Husk å fylle inn kontaktinfo, så du kan haven på Topplista, og være med i trekningen av fete premier.</p>
+      <input type="text" placeholder="noen@domene.no" onChange={(e) => setEmail(e.target.value)} />
+      <input type="text" placeholder="Navn eller kallenavn" onChange={(e) => setNickname(e.target.value)} />
+      <p>Vi sletter all info så raskt som mulig etter at vinnere er trukket og kontaktet. Du får ingen nyhetsbrev, vi lover!</p>
+      <ButtonSubmit value="Trykk her" disabled={!input} onClick={handleButtonClick} />
+    </div>
+
+  );
+}
 
 export const Ferdig = () => {
   const context = useContext(AppContext);
@@ -11,15 +61,16 @@ export const Ferdig = () => {
   return (
     <div className="centercolumn centercolumn--centeraligned">
       <Introduction>
-
-      <p>Du har nå meldt deg av nyhetsbrevet. (Gratulerer)</p>
+        <p>Du har nå meldt deg av nyhetsbrevet. (Gratulerer)</p>
       </Introduction>
-      <img src="/brukeropplevelse.png" alt="" />
+      <img src="/brukeropplevelse.png" alt="Illustrasjon av en mann som sklir ned en sklie." />
 
       <p>Din score:</p>
       <p className={styles.ferdig__time}>
         {context?.time} sekunder
       </p>
+      <Konkurranseskjema/>
+
       <img src="/Kantega_logo_hvit.svg" alt="Kantega" className={styles.ferdig__logo}/>
     </div>
   );
